c685ee7ecebdb1de7cb68f04525a541e9f9d4b84
zahidmiran <zahidmiron599@gmail.com>
2022-02-17 23:09:37 +0600

    all section added

--- js/management.js
+++ js/management.js
@@ -1,4 +1,3 @@
-
 // var calculateButton = 
 document.getElementById('calculate-btn').addEventListener('click', function () {
     // console.log('calculate button Clicked');
@@ -7,32 +6,17 @@ document.getElementById('calculate-btn').addEventListener('click', function () {
     var foodInputText = document.getElementById('food-input');
     var foodInputTk = parseFloat(foodInputText.value);
     // console.log(foodInputTk);
-
-
-
     var rentInputText = document.getElementById('rent-input');
     var rentInputTk = parseFloat(rentInputText.value);;
     // console.log(rentInputTk);
-
     var clothesInputText = document.getElementById('clothes-input');
     var clothesInputTk = parseFloat(clothesInputText.value);
     // console.log(clothesInputTk);
-
-
-
-
     // update food input tk to total expenses
-
     var totalExpenses = document.getElementById('expenses-total');
     const totalExpensesText = totalExpenses.innerText;
     const totalExpensesTk = parseFloat(totalExpensesText);
-
     const totalExpensesTkSum = totalExpensesTk + foodInputTk + rentInputTk + clothesInputTk;
-
-    totalExpenses.innerText = totalExpensesTkSum;
-
-
-
     // get the total income 
     const incomeInputText = document.getElementById('income-input');
     const incomeInputTk = parseFloat(incomeInputText.value);
@@ -41,21 +25,32 @@ document.getElementById('calculate-btn').addEventListener('click', function () {
     const balanceText = balance.innerText;
     const balanceTk = parseFloat(balanceText);
     // console.log('remaining balance is', balanceTk);
-
     const balanceTkAvailable = incomeInputTk - totalExpensesTkSum;
+    console.log('Abaliavle balance is', balanceTkAvailable);
 
-    balance.innerText = balanceTkAvailable;
-    console.log('remaining balance is', balanceTkAvailable);
+    if (totalExpensesTkSum <= incomeInputTk) {
+
+        totalExpenses.innerText = totalExpensesTkSum;
+
+        balance.innerText = balanceTkAvailable;
+
+    }
+    else if (totalExpensesTkSum > incomeInputTk) {
+        alert('You spend more than you earn and as a result you run the risk of becoming financially indebted. So Please reduce your Expenses for calculate this mission??')
+    }
 
 
+    //check the input field typeOf
 
+    if (isNaN(incomeInputTk)) {
+        alert('the alerat')
+    }
 
-
-    /* // clear food input text
+    // clear food input text
     foodInputText.value = '';
     rentInputText.value = '';
     clothesInputText.value = '';
-    totalExpenses.value = ''; */
+    totalExpenses.value = '';
 
 
 });
@@ -69,30 +64,58 @@ function getTheAmount(inputId) {
 }
 
 
+
+
+
+
 // work with bonus & save section
 document.getElementById('save-btn').addEventListener('click', function () {
-
-
     var saveInputText = document.getElementById('save-input');
     var saveInputTk = parseFloat(saveInputText.value);
-    // console.log(saveInputTk);
-    const balance = document.getElementById('balance-input');
+
+    const balance = document.getElementById('balance-total');
     const balanceText = balance.innerText;
     const balanceTk = parseFloat(balanceText);
-
-
     const saveWithbalanceTkAvailable = saveInputTk / 100 * balanceTk;
+    const savingAmount = getTheAmount('saving-amount');
+    const remainingBalance = getTheAmount('remaining-balance');
+    const availableBalanceText = document.getElementById('balance-total').innerText;
+    const availableBalance = parseFloat(availableBalanceText);
+
+    const calculateRemainingBalance = availableBalance - saveWithbalanceTkAvailable;
+
+
+
+
+
+    if (saveInputTk <= 100) {
+
+        const saveWithbalanceTkAvailable = saveInputTk / 100 * balanceTk;
+        document.getElementById('saving-amount').innerText = saveWithbalanceTkAvailable;
+        document.getElementById('remaining-balance').innerText = calculateRemainingBalance;
+
+        console.log('i can save lase then my available balance')
+    }
+    else if (saveInputTk > 100) {
+        alert('You Cannot Save More than Your Available Balance?? To save some money Please Input lase than 100% amount in this field?');
+    }
+
+
     console.log(saveWithbalanceTkAvailable);
 
-    /* 
-    const savingAmount = document.getElementById('savingAmount').innerText;
-    console.log(savingAmount);
- */
 
-    const savingAmount = getTheAmount('saving-amount');
+    // savingAmount.innerText = saveWithbalanceTkAvailable;
 
-    savingAmount.innerText = saveWithbalanceTkAvailable;
-    console.log('the saving amount is', savingAmount);
+    console.log('the saving amount is', saveWithbalanceTkAvailable);
+
+    // balanceTkAvailable
+
+    console.log('the available balance', availableBalance);
+
+
+
+    console.log('the remaining Balance', calculateRemainingBalance);
+
 
 
 });
